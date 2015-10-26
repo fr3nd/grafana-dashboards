6a2936a52d3c516283d9111710477514ae45b3ef
Carles Amigó <carles.amigo@socialpoint.es>
2015-10-26 18:13:12 +0100

    fixing typo

--- graphite.js
+++ graphite.js
@@ -308,7 +308,7 @@ function panel_collectd_interface(instance,default_panel){
     var name = interfaces[x].split(".")[1].replace("interface-", "");
 
     var panel_interface_octets = {
-      title: 'Interface traffic (' + name + ') on ' + host,
+      title: 'Interface traffic (' + name + ') on ' + instance,
       type: 'graph',
       grid: {max: null, min: null},
       aliasColors: {
