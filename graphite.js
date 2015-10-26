845342efef2a714171dbaf5a513e9bfddac75da5
Carles Amigó <carles.amigo@socialpoint.es>
2015-10-26 13:45:32 +0100

    adding first test for graphite dashboard

---
+++ graphite.js
@@ -0,0 +1,265 @@
+/* global _ */
+/*
+ * Complex scripted dashboard for CollectD + Graphite
+ * Author: Carles Amigó fr3nd@fr3nd.net
+ *
+ * based on:
+ * * http://anatolijd.blogspot.com.es/2014/07/scripting-grafana-dashboards.html
+ * * https://gist.github.com/mikepea/07b1cede92c119e4f297
+ */
+
+
+// accessible variables in this scope
+var window, document, ARGS, $, jQuery, moment, kbn;
+
+// use defaults for URL arguments
+var arg_i    = 'default';
+var arg_span = 4;
+var arg_from = '2h';
+var arg_datasource_url = '/api/datasources/proxy/2';
+
+if(!_.isUndefined(ARGS.span)) {
+  arg_span = ARGS.span;
+}
+
+if(!_.isUndefined(ARGS.i)) {
+  arg_i = ARGS.i;
+}
+
+if(!_.isUndefined(ARGS.from)) {
+  arg_from = ARGS.from;
+}
+
+if(!_.isUndefined(ARGS.datasource_url)) {
+  arg_datasource_url = ARGS.datasource_url;
+}
+
+// return dashboard filter_list
+// optionally include 'All'
+function get_filter_object(name,query,show_all){
+  show_all = (typeof show_all === "undefined") ? true : show_all;
+  var arr = find_filter_values(query);
+  var opts = [];
+  for (var i in arr) {
+    opts.push({"text":arr[i], "value":arr[i]});
+  }
+  if (show_all) {
+    opts.unshift({"text":"All", "value": '{'+arr.join()+'}'});
+  }
+  return {
+    type: "filter",
+      name: name,
+      query: query,
+      options: opts,
+      current: opts[0],
+      includeAll: show_all
+  };
+}
+
+// execute graphite-api /metrics/find query
+// return array of metric last names ( func('test.cpu-*') returns ['cpu-0','cpu-1',..] )
+function find_filter_values(query){
+  var search_url = window.location.protocol + '//' + window.location.host + arg_datasource_url + '/metrics/find/?query=' + query;
+  var res = [];
+  var req = new XMLHttpRequest();
+  req.open('GET', search_url, false);
+  req.send(null);
+  var obj = JSON.parse(req.responseText);
+  for(var key in obj) {
+    if (obj[key].hasOwnProperty("text")) {
+      res.push(obj[key].text);
+    }
+  }
+  return res;
+}
+
+// execute graphite-api /metrics/expand query
+// return array of metric full names (func('*.cpu-*') returns ['test.cpu-0','test.cpu-1',..] )
+function expand_filter_values(query){
+  var search_url = window.location.protocol + '//' + window.location.host + arg_datasource_url + '/metrics/expand/?query=' + query;
+  var req = new XMLHttpRequest();
+  req.open('GET', search_url, false);
+  req.send(null);
+  var obj = JSON.parse(req.responseText);
+  if (obj.hasOwnProperty('results')) {
+    return obj.results;
+  } else {
+    return [];
+  }
+}
+
+// used to calculate aliasByNode index in panel template
+function len(prefix){
+  return prefix.split('.').length - 2;
+}
+
+
+function panel_collectd_cpu(title,prefix){
+  var idx = len(prefix);
+  return {
+    title: title,
+      type: 'graphite',
+      span: arg_span,
+      renderer: "flot",
+      y_formats: ["none"],
+      grid: {max: null, min: 0},
+      lines: true,
+      fill: 1,
+      linewidth: 1,
+      stack: true,
+      legend: {show: true},
+      percentage: true,
+      nullPointMode: "null",
+      tooltip: {
+        value_type: "individual",
+        query_as_alias: true
+      },
+      targets: [
+      { "target": "alias(sumSeries(nonNegativeDerivative(" + prefix + "[[instance]].cpu-*.cpu-user,0)),'user')" },
+      { "target": "alias(sumSeries(nonNegativeDerivative(" + prefix + "[[instance]].cpu-*.cpu-system,0)),'system')" },
+      { "target": "alias(sumSeries(nonNegativeDerivative(" + prefix + "[[instance]].cpu-*.cpu-idle,0)),'idle')" },
+      { "target": "alias(sumSeries(nonNegativeDerivative(" + prefix + "[[instance]].cpu-*.cpu-wait,0)),'wait')" },
+      { "target": "alias(sumSeries(nonNegativeDerivative(" + prefix + "[[instance]].cpu-*.cpu-steal,0)),'steal')" },
+      { "target": "alias(sumSeries(nonNegativeDerivative(" + prefix + "[[instance]].cpu-*.cpu-nice,0)),'nice')" },
+      { "target": "alias(sumSeries(nonNegativeDerivative(" + prefix + "[[instance]].cpu-*.cpu-softirq,0)),'irq')" },
+      { "target": "alias(sumSeries(nonNegativeDerivative(" + prefix + "[[instance]].cpu-*.cpu-interrupt,0)),'intrpt')" },
+      ],
+      aliasColors: {
+        "user": "#508642",
+        "system": "#EAB839",
+        "wait": "#890F02",
+        "steal": "#E24D42",
+        "idle": "#6ED0E0"
+      }
+  };
+}
+
+function panel_collectd_network_octets(title,prefix,intrf){
+  intrf = (typeof intrf === "undefined") ? 'interface-eth0' : intrf;
+  var idx = len(prefix);
+  return {
+    title: title + ', ' + intrf,
+      type: 'graphite',
+      span: arg_span,
+      y_formats: ["bytes"],
+      grid: {max: null, min: null},
+      lines: true,
+      fill: 1,
+      linewidth: 2,
+      nullPointMode: "null",
+      targets: [
+      { "target": "aliasByNode(movingMedian(nonNegativeDerivative(keepLastValue(" + prefix + "[[instance]]." + intrf + ".if_octets.rx,10),0),'5min')," +(idx+4)+ ")" },
+      { "target": "aliasByNode(movingMedian(scale(nonNegativeDerivative(keepLastValue(" + prefix + "[[instance]]." + intrf + ".if_octets.tx,10),0),-1),'5min')," +(idx+4)+ ")" }
+    ]
+  };
+}
+
+
+function row_cpu_memory(title,prefix){
+  return {
+    title: title,
+      height: '250px',
+      collapse: false,
+      panels: [
+        panel_collectd_cpu('CPU, %',prefix),
+      panel_collectd_memory('Memory',prefix),
+      panel_collectd_loadavg('Load avg, 10min',prefix)
+        ]
+  };
+}
+
+
+function row_network(title,prefix,filter){
+  var interfaces = find_filter_values(filter + '.interface-*');
+  var panels_network = [];
+  for (var i in interfaces) {
+    panels_network.push(
+        panel_collectd_network_octets('network octets',prefix,interfaces[i]),
+        panel_collectd_network_packets('network packets',prefix,interfaces[i])
+        );
+  }
+  return {
+    title: title,
+      height: '250px',
+      collapse: true,
+      panels: panels_network
+  };
+}
+
+
+return function(callback) {
+
+  // Setup some variables
+  var dashboard, timspan;
+
+  var prefix = '';
+
+  var arg_filter = prefix + arg_i;
+
+  // set filter
+  var dashboard_filter = {
+    time: {
+      from: "now-" + arg_from,
+      to: "now"
+    },
+    list: [
+      get_filter_object("instance",arg_filter,false)
+      ]
+  };
+
+  // define pulldowns
+  pulldowns = [
+  {
+    type: "filtering",
+      collapse: false,
+      notice: false,
+      enable: true
+  },
+  {
+    type: "annotations",
+    enable: false
+  }
+  ];
+
+  // Intialize a skeleton with nothing but a rows array and service object
+
+  dashboard = {
+    rows : [],
+    services : {}
+  };
+  dashboard.title = arg_i;
+      dashboard.editable = true;
+      dashboard.pulldowns = pulldowns;
+      dashboard.services.filter = dashboard_filter;
+
+
+      // custom dashboard rows (appended to the default dashboard rows)
+
+      var optional_rows = [];
+
+      $.ajax({
+        method: 'GET',
+        url: '/'
+      })
+      .done(function(result) {
+
+        // construct dashboard rows
+
+        dashboard.rows.push(
+          row_cpu_memory('cpu, memory',prefix),
+          row_swap('swap',prefix),
+          row_network('network',prefix,arg_filter),
+          row_disk_space('disk space',prefix,arg_filter),
+          row_disk_usage('disk ops',prefix,arg_filter)
+          );
+
+        // custom rows
+        for (var i in optional_rows){
+          dashboard.rows.push(optional_rows[i]);
+        }
+
+        // when dashboard is composed call the callback
+        // function and pass the dashboard
+        callback(dashboard);
+      });
+};
